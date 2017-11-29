c94eeb40113fe307ce57a427c47a87705009284d
Junaid <md.junaid028@gmail.com>
2017-11-30 00:42:24 +0530

    deploy changes for heroku

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require("express"),
 	app		= express();
 
-app.set('port', 3000);
+// app.set('port', 3000);
 
 app.use(express.static(__dirname + "/docs"));
 
-app.get("/", function(req, res) {
-	res.render("index");
-});
+// app.get("/", function(req, res) {
+// 	res.render("index");
+// });
 
-var server = app.listen(app.get('port'), function() {
-	var port = server.address().port;
-	console.log("server started at port: " + port);
+// var server = app.listen(app.get('port'), function() {
+// 	var port = server.address().port;
+// 	console.log("server started at port: " + port);
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started!!");
 });
